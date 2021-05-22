e0af0486aef5a9f81ac806ecd8dac17b7c865268
Furkan Baldır <36567819+supremepanda@users.noreply.github.com>
2021-05-22 18:02:57 +0300

    UI TAB navigation added to login screen.

---
+++ Assets/Scripts/Login/UITabNavigation.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+/// <summary>
+/// This class just controlling navigation with keyboard "TAB" key. With this class, we can navigate the UI with TAB key.
+/// </summary>
+public class UITabNavigation : MonoBehaviour
+{
+    EventSystem system;
+
+    void Start()
+    {
+        system = EventSystem.current;
+
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            Selectable next = system.currentSelectedGameObject.GetComponent<Selectable>().FindSelectableOnDown();
+
+            if (next != null)
+            {
+
+                InputField inputfield = next.GetComponent<InputField>();
+                if (inputfield != null)
+                    inputfield.OnPointerClick(new PointerEventData(system));
+
+                system.SetSelectedGameObject(next.gameObject, new BaseEventData(system));
+            }
+
+        }
+    }
+}
